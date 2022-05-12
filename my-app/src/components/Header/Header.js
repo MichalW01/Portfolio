bdc7af7d1ff2c5dbecd410d574ae74c2d79b26fa
Michał Wołodźko <michal.wolodzko@gmail.com>
2022-05-12 18:50:19 +0200

    add scss, add styles

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Header.scss';
+
+function Header({}) {
+  return (
+    <header className="Header">
+     Hello! I'm Mike
+    </header>
+  );
+}
+
+export default Header;
